e2e43bc54cb79cad91d4c023a653c28a2d8673aa
Roman Tretyak <work.romanuel@yandex.ru>
2019-03-23 14:27:14 +0300

    Fix SSlError

--- asyncer.py
+++ asyncer.py
@@ -10,6 +10,7 @@ class Asyncer:
     """
     Основной и единственный класс в модуле.
     """
+
     def __init__(self, func):
         """
         Конструктор класса.
@@ -36,35 +37,40 @@ class Asyncer:
         return await asyncio.gather(*tasks)
 
     @classmethod
-    async def _fetch(cls, session, url, mode='text'):
+    async def _fetch(cls, url, mode='text'):
         """
         Асинхронный запрос по адресу url.
         :param session: сессия.
         :param url: адрес.
-        :return: результат запроса.
+        :return: Адрес и результат запроса.
         """
-        print(url)
-        async with session.get(url) as response:
-            if mode == 'json':
-                return await response.json()
-            else:
-                return await response.text()
+        try:
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+                async with session.get(url) as response:
+                    if mode == 'json':
+                        resp = await response.json()
+                    else:
+                        resp = await response.text()
+                    print('{} - {}'.format(url, 'done'))
+                    return {'url': url, 'response': resp}
+        except:
+            print('{} - {}'.format(url, 'FAIL'))
+            return {'url': url, 'response': None}
 
     @classmethod
-    async def _fetcher(cls, urls, mode):
+    async def _fetcher(cls, urls, mode='text'):
         """
         Создание списка запросов.
         :param: список адресов.
         :return: спискок результатов запросов.
         """
         tasks = []
-        async with aiohttp.ClientSession() as session:
-            for url in urls:
-                tasks.append(asyncio.ensure_future(cls._fetch(session, url, mode)))
-            return await asyncio.gather(*tasks)
+        for url in urls:
+            tasks.append(asyncio.ensure_future(cls._fetch(url, mode)))
+        return await asyncio.gather(*tasks)
 
     @classmethod
-    def async_fetch(cls, urls, mode):
+    def async_fetch(cls, urls, mode='text'):
         """
         Отправка асинхронных запросов по адресам urls.
         :param mode: тип ответа, json или text, по умолчанию text.
